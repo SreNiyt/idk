923d7c9ecd7c2a93fc85f8ee92a3092517fc8a80
SreNiyt <180774411+SreNiyt@users.noreply.github.com>
2026-05-02 17:29:29 +0700

    Restore missing IsNull and constructor methods to ScopedLocalRef

--- Bcore/pine-core/src/main/cpp/utils/scoped_local_ref.h
+++ Bcore/pine-core/src/main/cpp/utils/scoped_local_ref.h
@@ -7,72 +7,50 @@
 template<typename T>
 class ScopedLocalRef {
 public:
-    ScopedLocalRef(JNIEnv* env) : env_(env), ref_(nullptr) {
-    }
-
-    ScopedLocalRef(JNIEnv* env, T ref) : env_(env), ref_(ref) {
-    }
-
-    ~ScopedLocalRef() {
-        if (ref_ != nullptr) {
-            env_->DeleteLocalRef(ref_);
-        }
-    }
-
+    ScopedLocalRef(JNIEnv* env) : env_(env), ref_(nullptr) {}
+    ScopedLocalRef(JNIEnv* env, T ref) : env_(env), ref_(ref) {}
+    ~ScopedLocalRef() { if (ref_ != nullptr) env_->DeleteLocalRef(ref_); }
+    
+    T get() const { return ref_; }
+    bool IsNull() const { return ref_ == nullptr; }
+    
     void reset(T ptr = nullptr) {
         if (ptr != ref_) {
-            if (ref_ != nullptr) {
-                env_->DeleteLocalRef(ref_);
-            }
+            if (ref_ != nullptr) env_->DeleteLocalRef(ref_);
             ref_ = ptr;
         }
     }
+    
+    bool operator==(std::nullptr_t) const { return ref_ == nullptr; }
+    bool operator!=(std::nullptr_t) const { return ref_ != nullptr; }
 
-    T get() const {
-        return ref_;
-    }
-
-    bool operator==(std::nullptr_t) const {
-        return ref_ == nullptr;
-    }
-
-    bool operator!=(std::nullptr_t) const {
-        return ref_ != nullptr;
-    }
-
-private:
+protected:
     JNIEnv* const env_;
     T ref_;
 
+private:
     ScopedLocalRef(const ScopedLocalRef&) = delete;
     void operator=(const ScopedLocalRef&) = delete;
 };
 
 class ScopedLocalClassRef : public ScopedLocalRef<jclass> {
 public:
-    ScopedLocalClassRef(JNIEnv* env) : ScopedLocalRef<jclass>(env) {
-    }
-
-    ScopedLocalClassRef(JNIEnv* env, jclass ref) : ScopedLocalRef<jclass>(env, ref) {
-    }
+    ScopedLocalClassRef(JNIEnv* env) : ScopedLocalRef<jclass>(env) {}
+    ScopedLocalClassRef(JNIEnv* env, jclass ref) : ScopedLocalRef<jclass>(env, ref) {}
+    // The missing string constructor:
+    ScopedLocalClassRef(JNIEnv* env, const char* name) : ScopedLocalRef<jclass>(env, env->FindClass(name)) {}
 };
 
 class ScopedLocalObjectRef : public ScopedLocalRef<jobject> {
 public:
-    ScopedLocalObjectRef(JNIEnv* env) : ScopedLocalRef<jobject>(env) {
-    }
-
-    ScopedLocalObjectRef(JNIEnv* env, jobject ref) : ScopedLocalRef<jobject>(env, ref) {
-    }
+    ScopedLocalObjectRef(JNIEnv* env) : ScopedLocalRef<jobject>(env) {}
+    ScopedLocalObjectRef(JNIEnv* env, jobject ref) : ScopedLocalRef<jobject>(env, ref) {}
 };
 
 class ScopedLocalUtfStringRef : public ScopedLocalRef<jstring> {
 public:
-    ScopedLocalUtfStringRef(JNIEnv* env) : ScopedLocalRef<jstring>(env) {
-    }
-
-    ScopedLocalUtfStringRef(JNIEnv* env, jstring ref) : ScopedLocalRef<jstring>(env, ref) {
-    }
+    ScopedLocalUtfStringRef(JNIEnv* env) : ScopedLocalRef<jstring>(env) {}
+    ScopedLocalUtfStringRef(JNIEnv* env, jstring ref) : ScopedLocalRef<jstring>(env, ref) {}
 };
 
 #endif //PINE_UTILS_SCOPED_LOCAL_REF_H
